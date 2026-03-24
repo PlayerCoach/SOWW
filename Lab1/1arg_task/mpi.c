990132254775825be6cd34bfdc6ced1df5a14a02
Player/Coach <olafinski14@gmail.com>
2026-03-24 12:25:27 +0100

    Changed folder structure

---
+++ Lab1/1arg_task/mpi.c
@@ -0,0 +1,43 @@
+#include "utility.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <mpi.h>
+
+int main(int argc,char **argv) {
+
+  Args ins__args;
+  parseArgs(&ins__args, &argc, argv);
+
+  //program input argument
+  long inputArgument = ins__args.arg; 
+
+  struct timeval ins__tstart, ins__tstop;
+
+  int myrank,nproc;
+  
+  MPI_Init(&argc,&argv);
+
+  // obtain my rank
+  MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+  // and the number of processes
+  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+
+  if(!myrank)
+      gettimeofday(&ins__tstart, NULL);
+
+
+  // run your computations here (including MPI communication)
+
+
+
+  // synchronize/finalize your computations
+
+  if (!myrank) {
+    gettimeofday(&ins__tstop, NULL);
+    ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
+  }
+  
+  MPI_Finalize();
+
+}
